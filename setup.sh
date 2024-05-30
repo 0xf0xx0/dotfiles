a9fa84e8bb3a73e367c82ed64b9f104a90496472
Gingka/Ginger Pepper <33764485+ExperiBass@users.noreply.github.com>
2024-05-29 20:22:44 -0400

    install omz in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@
 set -eu
 root=`dirname "$0"`
 
+ln -fbrs "$root/.zshrc" "$HOME/.zshrc"
+
 ln -rs "$root/.bin" "$HOME/.bin"
 ln -rs "$root/.config/*" "$XDG_CONFIG_HOME/.config/"
 
 ln -rs "$root/.local/share/themes/*" "$XDG_DATA_HOME/themes/"
 ln -rs "$root/.local/share/icons/*" "$XDG_DATA_HOME/icons/"
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
