7b2c1ae9f4a009d3dd2bfae3c8a77f568b3b6108
giing <33764485+ExperiBass@users.noreply.github.com>
2024-10-28 21:22:14 -0400

    rofi pt5

--- .bin/powermenu.sh
+++ .bin/powermenu.sh
@@ -4,13 +4,9 @@
 ## Github : @adi1090x
 #
 ## Rofi   : Power Menu
-#
-## Available Styles
-#
-## style-1   style-2   style-3   style-4   style-5
 
 # Current Theme
-dir="$HOME/.config/rofi/powermenu/type-3"
+dir="$HOME/.config/rofi/powermenu/"
 theme='style-giing'
 
 # CMDs
